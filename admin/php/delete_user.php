3d762d0fcdf2a2bd2a2701550ad9f5a095582189
joel B.Báez <129115720+Kokuuuuuun@users.noreply.github.com>
2025-04-21 21:26:36 +0000

    Update delete_user.php

--- admin/php/delete_user.php
+++ admin/php/delete_user.php
@@ -3,11 +3,11 @@ include("conexion.php");
 
 $id = $_GET['id'];
 $sql = "DELETE FROM usuario WHERE id = ?";
-$stmt = mysqli_prepare($connection, $sql);
+$stmt = mysqli_prepare($conexion, $sql);
 mysqli_stmt_bind_param($stmt, "i", $id);
 $success = mysqli_stmt_execute($stmt);
 
-mysqli_close($connection);
+mysqli_close($conexion);
 
 header("Location: usuario-admin.php");
 exit();
