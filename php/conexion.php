7eab7fa79b97365685d6cb9538bd446e79dc6519
joel B.Báez <129115720+Kokuuuuuun@users.noreply.github.com>
2025-05-01 14:55:58 -0400

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
 // Configuración de la conexión a la base de datos
-$host = "10.0.1.9";    // Dirección IP del servidor MySQL
+$host = "10.0.1.6";    // Dirección IP del servidor MySQL
 $usuario = "root";       // Usuario de MySQL
-$password = "1234567890"; // Contraseña de MySQL
+$password = ""; // Contraseña de MySQL
 $base_datos = "prime";   // Nombre de la base de datos
 $puerto = 3306;          // Puerto de MySQL
 
